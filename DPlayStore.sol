ebbbd573dc401fb2f887e8e27cffa6b516086ea4
Hanul <hanul@hanul.me>
2019-09-16 21:44:48 +0900

    평가 부분을 분리합니다.

--- DPlayStore.sol
+++ DPlayStore.sol
@@ -8,8 +8,6 @@ import "./Util/SafeMath.sol";
 contract DPlayStore is DPlayStoreInterface, NetworkChecker {
 	using SafeMath for uint;
 	
-	uint8 constant private RATING_DECIMALS = 18;
-	
 	Game[] private games;
 	
 	mapping(address => uint[]) private publisherToGameIds;
@@ -17,9 +15,6 @@ contract DPlayStore is DPlayStoreInterface, NetworkChecker {
 	
 	mapping(address => uint[]) private buyerToGameIds;
 	mapping(uint => address[]) private gameIdToBuyers;
-	
-	mapping(address => uint[]) private raterToGameIds;
-	mapping(uint => Rating[]) private gameIdToRatings;
 	
 	DPlayCoinInterface private dplayCoin;
 	
@@ -38,10 +33,6 @@ contract DPlayStore is DPlayStoreInterface, NetworkChecker {
 		} else {
 			revert();
 		}
-	}
-	
-	function ratingDecimals() external view returns (uint8) {
-		return RATING_DECIMALS;
 	}
 	
 	// Returns the number of games.
@@ -323,129 +314,5 @@ contract DPlayStore is DPlayStoreInterface, NetworkChecker {
 	// 특정 구매자가 구매한 게임 ID들을 가져옵니다.
 	function getBoughtGameIds(address buyer) external view returns (uint[] memory) {
 		return buyerToGameIds[buyer];
-	}
-	
-	// Rates the game.
-	// 게임을 평가합니다.
-	function rate(uint gameId, uint rating, string calldata review) external {
-		
-		Game memory game = games[gameId];
-		
-		// The game info must be normal.
-		// 정상적인 게임 정보여야 합니다.
-		require(game.publisher != address(0x0));
-		
-		// Paid games can only be rated buy their buyers.
-		// 유료 게임은 구매자만 평가할 수 있습니다.
-		require(game.price == 0 || checkIsBuyer(msg.sender, gameId) == true);
-		
-		// The sender can rate the game only once.
-		// 이미 평가한 경우에는 재평가할 수 없습니다.
-		require(checkIsRater(msg.sender, gameId) != true);
-		
-		// The rating must be 10 or lower.
-		// 점수는 10점 이하여야 합니다.
-		require(rating <= 10 * 10 ** uint(RATING_DECIMALS));
-		
-		// Registers the message sender as a rater.
-		// 평가자로 등록합니다.
-		raterToGameIds[msg.sender].push(gameId);
-		
-		// Registers the rating.
-		// 평가를 등록합니다.
-		gameIdToRatings[gameId].push(Rating({
-			rater : msg.sender,
-			rating : rating,
-			review : review
-		}));
-		
-		emit Rate(gameId, msg.sender, rating, review);
-	}
-	
-	// Checks if the given address is the rater's address.
-	// 특정 주소가 평가자인지 확인합니다.
-	function checkIsRater(address addr, uint gameId) public view returns (bool) {
-		
-		uint[] memory gameIds = raterToGameIds[addr];
-		for (uint i = 0; i < gameIds.length; i += 1) {
-			if (gameIds[i] == gameId) {
-				return true;
-			}
-		}
-		
-		return false;
-	}
-	
-	// Returns the rating info of the given rater.
-	// 특정 평가자가 내린 평가 정보를 반환합니다.
-	function getRating(address rater, uint gameId) external view returns (uint rating, string memory review) {
-		
-		Rating[] memory ratings = gameIdToRatings[gameId];
-		for (uint i = 0; i < ratings.length; i += 1) {
-			
-			// Finds the rating rated by the rater.
-			// 특정 평가자가 내린 평가인 경우
-			if (ratings[i].rater == rater) {
-				return (
-					ratings[i].rating,
-					ratings[i].review
-				);
-			}
-		}
-	}
-	
-	// Gets the game IDs rated by the given rater.
-	// 특정 평가자가 평가한 게임 ID들을 가져옵니다.
-	function getRatedGameIds(address rater) external view returns (uint[] memory) {
-		return raterToGameIds[rater];
-	}
-	
-	// Updates a rating.
-	// 평가를 수정합니다.
-	function updateRating(uint gameId, uint rating, string calldata review) external {
-		
-		Rating[] storage ratings = gameIdToRatings[gameId];
-		for (uint i = 0; i < ratings.length; i += 1) {
-			
-			// The sender must be the rater.
-			// 평가자인 경우에만
-			if (ratings[i].rater == msg.sender) {
-				
-				ratings[i].rating = rating;
-				ratings[i].review = review;
-				
-				emit UpdateRating(gameId, msg.sender, rating, review);
-				return;
-			}
-		}
-	}
-	
-	// Returns the number of ratings of a game.
-	// 게임의 평가 수를 반환합니다.
-	function getRatingCount(uint gameId) external view returns (uint) {
-		return gameIdToRatings[gameId].length;
-	}
-	
-	// Returns the overall rating of a game.
-	// Overall rating = (The sum of all weighted ratings : Each rater's DC Power * Each rater's rating) / Sum of each rater's DC Power
-	// 게임의 종합 평가 점수를 반환합니다.
-	// 종합 평가 점수 = (모든 평가의 합: 평가자 A의 DC Power * 평가자 A의 평가 점수) / 모든 평가자의 DC Power
-	function getOverallRating(uint gameId) external view returns (uint) {
-		
-		uint totalPower = 0;
-		uint totalRating = 0;
-		
-		Rating[] memory ratings = gameIdToRatings[gameId];
-		for (uint i = 0; i < ratings.length; i += 1) {
-			if (ratings[i].rater != address(0x0)) {
-				
-				uint power = dplayCoin.getPower(ratings[i].rater);
-				
-				totalPower = totalPower.add(power);
-				totalRating = totalRating.add(power.mul(ratings[i].rating));
-			}
-		}
-		
-		return totalPower == 0 ? 0 : totalRating.div(totalPower);
 	}
 }
--- DPlayStoreInterface.sol
+++ DPlayStoreInterface.sol
@@ -9,9 +9,6 @@ interface DPlayStoreInterface {
     event Release(uint indexed gameId);
     event Unrelease(uint indexed gameId);
     event Buy(uint indexed gameId, address indexed buyer);
-    event Rate(uint indexed gameId, address indexed rater, uint rating, string review);
-    event UpdateRating(uint indexed gameId, address indexed rater, uint rating, string review);
-    event RemoveRating(uint indexed gameId, address indexed rater);
     
     // Game info
     // 게임 정보
@@ -36,16 +33,6 @@ interface DPlayStoreInterface {
 		string	titleImageURL;
 		string	bannerImageURL;
 	}
-	
-	// Rating info
-	// 평가 정보
-	struct Rating {
-		address	rater;
-		uint	rating;
-		string	review;
-	}
-	
-	function ratingDecimals() external view returns (uint8);
 	
 	// Returns the number of games.
 	// 게임의 개수를 반환합니다.
@@ -129,34 +116,4 @@ interface DPlayStoreInterface {
 	// Gets the IDs of the games bought by the given buyer.
 	// 특정 구매자가 구매한 게임 ID들을 가져옵니다.
 	function getBoughtGameIds(address buyer) external view returns (uint[] memory);
-	
-	// Rates a game.
-	// 게임을 평가합니다.
-	function rate(uint gameId, uint rating, string calldata review) external;
-	
-	// Checks if the given address is the rater's address.
-	// 특정 주소가 평가자인지 확인합니다.
-	function checkIsRater(address addr, uint gameId) external view returns (bool);
-	
-	// Gets the game IDs rated by the given rater.
-	// 특정 평가자가 평가한 게임 ID들을 가져옵니다.
-	function getRatedGameIds(address rater) external view returns (uint[] memory);
-	
-	// Returns the rating info of the given rater.
-	// 특정 평가자가 내린 평가 정보를 반환합니다.
-	function getRating(address rater, uint gameId) external view returns (uint rating, string memory review);
-	
-	// Updates a rating.
-	// 평가를 수정합니다.
-	function updateRating(uint gameId, uint rating, string calldata review) external;
-	
-	// Returns the number of ratings of a game.
-	// 게임의 평가 수를 반환합니다.
-	function getRatingCount(uint gameId) external view returns (uint);
-	
-	// Returns the overall rating of a game.
-	// Overall rating = (The sum of all weighted ratings : Each rater's DC Power * Each rater's rating) / Sum of each rater's DC Power
-	// 게임의 종합 평가 점수를 반환합니다.
-	// 종합 평가 점수 = (모든 평가의 합: 평가자 A의 DC Power * 평가자 A의 평가 점수) / 모든 평가자의 DC Power
-	function getOverallRating(uint gameId) external view returns (uint);
 }
